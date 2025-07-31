5eeca9055c7b6c0fa8819389ffde196f8fc50f47
Rassya Maulana Handoyo <rassyahandoyo@icloud.com>
2025-07-31 12:38:51 +0700

    routes & app.js done

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const session = require('express-session');
-const flash = require('connect-flash');
-const path = require('path');
 const app = express();
 const router = require('./routes');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(session({
+    secret: 'coffeesecret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
+
+app.use(router);
+
+app.listen(3000, () => {
+    console.log('App running at http://localhost:3000');
+});
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,17 @@
 function isLoggedIn(req, res, next) {
     if (!req.session.userId) {
-        req.flash('error', 'You must log in first!');
-        return res.redirect('/login');
+        req.flash('error', 'You must log in first!')
+        return res.redirect('/login')
     }
     next();
 }
 
 function isAdmin(req, res, next) {
     if (req.session.role !== 'admin') {
-        req.flash('error', 'Only admin can access this page!');
-        return res.redirect('/');
+        req.flash('error', 'Only admin can access this page!')
+        return res.redirect('/')
     }
     next();
 }
 
-module.exports = {
-    isLoggedIn,
-    isAdmin
-};
+module.exports = { isLoggedIn, isAdmin }
